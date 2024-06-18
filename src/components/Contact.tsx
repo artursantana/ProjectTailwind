49734aee21c6cf9f21f10619a57bf7c48b41ea52
artursantana <artursantana843@gmail.com>
2024-06-18 10:31:57 +0100

    contacto finalizado

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 
 
-
+import Image from 'next/image'
 import React from 'react'
 
 const Contact = () => {
@@ -12,11 +12,18 @@ const Contact = () => {
         <p className='text-[17px] mt-[0.8rem] mb-[1.5rem] opacity-70'>We&apos;ll email you a voucher worth 10$ off your first order over 50$</p>
         <div className='w-[100%] relative h-[2.7rem] mb-[0.3rem] bg-white'>
             <input type="email" placeholder='Enter your Email' className='w-[70%] md:w-[79%] bg-white ml-4 outline-none h-[2.7rem]' />
-            <button className='w-[20%] md:w-[25%] right-0 absolute h-[2.7rem] bg-blue-500 hover:bg-blue-700 transition-all'>Subscribe</button>
+            <button className='w-[20%] md:w-[25%] md:text-[1px] right-0 absolute h-[2.7rem] bg-blue-500 hover:bg-blue-700 transition-all'>Subscribe</button>
             <p className='text-[14px] mt-[0.2rem] text-white opacity-35 font-thin'>By subscribing you agree to our terms & conditios and privacy & cookies policy</p>
         </div>
         </div>
-        
+        <div className='mt-[2rem] md:mt-0'>
+            <h1 className='text-[20px] md:text-[25px] lg:text-[30px] leading-[2.4rem]'>Need Help? <br /> +351 956 855 477</h1>
+            <p className='font-bold opacity-65 mt-[0.5rem]'>We are available 8:00am - 7:00pm</p>
+            <div className='mt-[2rem] flex items-center space-x-2'>
+                <Image width={130} height={130} className='object-contain' alt='playstore' src='/images/gp.png'/>
+                <Image width={130} height={130} className='object-contain' alt='appstore' src='/images/as.png'/>
+            </div>
+        </div>
       </div>
     </div>
   )
